4dd1167c10211710957925b27d1c1bdf69eae454
Nayra Rios <nayrios2004@gmail.com>
2024-07-28 11:20:17 +0200

    todobien

--- front/src/components/Main/Lateral/MisPublis.jsx
+++ front/src/components/Main/Lateral/MisPublis.jsx
@@ -2,19 +2,8 @@ import { useState } from "react";
 
 
 function MisPublis() {
-
-    const [contador, setContador] = useState(0);
-
-    const incrementarContador = () => {
-        setContador(contador + 1);
-        console.log(contador);
-      };
-    return (
-      <>
-
-       <button onClick={incrementarContador}>Incrementar</button>
-       <h1>Contador: {contador}</h1>
-
-      </>
+    (
+       <>
+       </>
     )
   } export default MisPublis;
