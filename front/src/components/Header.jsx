2d3d7eb95d0323d286e3899d22f93f566357205f
adrianvicoh <1hiaw.vico.adrian@gmail.com>
2024-07-29 16:30:23 +0200

    Añadidas primeras vistas y controller

---
+++ front/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Navbar, Nav, Container, NavDropdown, Row } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import './Header.css'
+
+function Header() {
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Header
