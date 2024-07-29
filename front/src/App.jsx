19da250f4255a0dae5b0d80fd26332b699a411e0
Adrián Vico <adrian.vico.95@gmail.com>
2024-07-29 16:30:23 +0200

    Añadidas primeras vistas y controller

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,12 @@
-import Header from './components/Header/Header'
+import Header from './components/Header.jsx'
+import Lateral from './components/Lateral'
 function App() {
   
   return (
     <>
-     <Header/>
-     
+     <Header />
+     <Lateral />
+     <Outlet />
     </>
   )
 }
---
+++ front/src/Home.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import CarruselHeader from './components/CarruselHeader';
+import CarruselMarca from './components/CarruselMarca';
+import BaseController from './controllers/BaseController';
+import { Container, Row, Col } from 'react-bootstrap';
