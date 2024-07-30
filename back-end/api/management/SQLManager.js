28445bdfffd2dadb3139a998b238e3850b51bb42
Algorhythmics <intivioli@gmail.com>
2024-07-30 19:56:29 +0200

    Update.

--- back-end/api/management/SQLManager.js
+++ back-end/api/management/SQLManager.js
@@ -1,5 +1,7 @@
 import fitmatch from "./../Fitmatch.js";
 
+const TABLES_VERSION = 0;
+
 class SQLManager {
     constructor() {}
 
@@ -32,11 +34,24 @@ class SQLManager {
     }
 
     createNewUser(name, lastname, email, phone, hash) {
-        return fitmatch.getSql().query(`INSERT INTO users(name, lastname, email, phone, pwhash) VALUES("${name}", "${lastname}", "${email}", "${phone}", "${hash}");`)
+        return fitmatch.getSql().query(`INSERT INTO users(name, lastname, email, phone, pwhash, tableVersion) VALUES("${name}", "${lastname}", "${email}", "${phone}", "${hash}", ${TABLES_VERSION});`)
     }
 
     updateUser(user) {
-        return fitmatch.getSql().query(`UPDATE users SET name = "${user.name}", lastname = "${user.lastname}", phone = "${user.phone}", description = "${user.description}", proficiency = "${user.proficiency}", trainingPreferences = "${user.trainingPreferences}", img = "${user.img}", location = "${user.city}||${user.coordinates}", isSetup = ${user.isSetup} WHERE id = ${user.id}`)
+        return fitmatch.getSql().query(`UPDATE users SET name = "${user.name}", lastname = "${user.lastname}", phone = "${user.phone}", description = "${user.description}", proficiency = "${user.proficiency}", trainingPreferences = "${user.trainingPreferences}", img = "${user.img}", location = "${user.city}||${user.latitude};${user.longitude}", isSetup = ${user.isSetup}, tableVersion = ${TABLES_VERSION} WHERE id = ${user.id}`)
+    }
+
+    selectivelyUpdateUser(user, map) {
+        const str = "";
+        let i = 0;
+        map.forEach((value, key) => {
+            str += `${key} = "${value}"`;
+            if (i !== 0 && i + 1 !== map.size) {
+                str += str += ", "
+            }
+            i++;
+        });
+        return fitmatch.getSql().query(`UPDATE user SET ${str} WHERE id = ${user.id}`);
     }
 }
 
