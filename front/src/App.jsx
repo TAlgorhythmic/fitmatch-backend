6808812173fa10fd0b71e7303a0c1aea8dcfd481
Nayra Rios <nayrios2004@gmail.com>
2024-07-31 19:52:30 +0200

    conexion basededatos

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,5 @@
 import Header from './components/Header.jsx'
 import { Outlet } from "react-router-dom";
-import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
