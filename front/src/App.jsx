0dc2a02eb87cc3905c7da05293fd3b62c7ca880c
adrianvicoh <1hiaw.vico.adrian@gmail.com>
2024-08-02 18:30:04 +0200

    Reorganizado layout

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header.jsx';
 import { Outlet } from "react-router-dom";
+import {Container} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -7,8 +8,10 @@ function App() {
   return (
     <>
       <div className="contenedorPrincipal">
-        <Header />
-        <Outlet />
+        <Container fluid="lg">
+          <Header />
+          <Outlet />
+        </Container>
       </div>
     </>
   )
