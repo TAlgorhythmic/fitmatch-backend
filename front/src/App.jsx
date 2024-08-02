596575776d79ad2fb0e362a74cb7396ffabc8706
G2Yang <yangtrabajos@gmail.com>
2024-08-02 17:18:26 +0200

    ahora si que si el validate

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,8 +1,44 @@
 import Header from './components/Header.jsx';
-import { Outlet } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
 function App() {
+  const [isValidToken, setIsValidToken] = useState(null);
+  const token = localStorage.getItem('authToken');
+
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        const response = await fetch('/api/validate-token', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        const data = await response.json();
+        setIsValidToken(data.valid);
+      } catch (error) {
+        console.error('Error validating token', error);
+        setIsValidToken(false);
+      }
+    };
+
+    if (token) {
+      validateToken();
+    } else {
+      setIsValidToken(false);
+    }
+  }, [token]);
+
+  if (isValidToken === null) {
+    // Renderizar un indicador de carga mientras se valida el token
+    return <div>Loading...</div>;
+  }
+
+  if (isValidToken === false) {
+    return <Navigate to="/login" />;
+  }
 
   return (
     <>
@@ -11,7 +47,7 @@ function App() {
         <Outlet />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
