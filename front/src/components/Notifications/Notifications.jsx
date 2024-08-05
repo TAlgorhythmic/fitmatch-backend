6372678e5618c83ead6c06dbce6c315ff1f638ad
G2Yang <yangtrabajos@gmail.com>
2024-08-05 16:13:18 +0200

    idk

---
+++ front/src/components/Notifications/Notifications.jsx
@@ -0,0 +1,13 @@
+function Notifications() {
+    const [notifications, setNotifications] = useState([]);
+
+    return (
+        <>
+            <div style={{width: '100%', height: '100vh', backgroundColor: 'red'}}>
+                
+            </div>
+        </>
+    )
+}
+
+export default Notifications;
