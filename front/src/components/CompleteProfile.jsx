2f9d4e0167f01880bdf695de6939b02ab287d565
Nayra Rios <nayrios2004@gmail.com>
2024-08-05 13:26:40 +0200

    UserProfile (editar datos)

---
+++ front/src/components/CompleteProfile.jsx
@@ -0,0 +1,16 @@
+/*
+Formulario que te sale al registrarte por primera vez
+APP googleMaps
+
+*/
+
+function CompleteProfileUser() {
+  
+    return (
+       <>
+
+       </>
+    );
+}
+
+export default CompleteProfileUser;
