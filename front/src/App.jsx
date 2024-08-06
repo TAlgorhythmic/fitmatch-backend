240af6cda2a65770a8714dcdce15b7ac3c6effd4
Adrián Vico <adrian.vico.95@gmail.com>
2024-08-06 16:05:23 +0200

    commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header/Header.jsx';
 import { Container } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Outlet } from 'react-router-dom';
+import SubHeader from './components/Header/SubHeader.jsx';
 
 function App() {
 
