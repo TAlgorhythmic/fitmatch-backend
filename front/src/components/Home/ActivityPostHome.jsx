2fe786a5490bc9ada34cca03f9fe9f4b94919807
Adrián Vico <adrian.vico.95@gmail.com>
2024-08-07 19:09:43 +0200

    Arreglado alert post y añadido icono

--- front/src/components/Home/ActivityPostHome.jsx
+++ front/src/components/Home/ActivityPostHome.jsx
@@ -1,8 +1,12 @@
 import { Alert, Row, Col, Image } from 'react-bootstrap';
+import { CheckCircleFill, CheckCircle } from 'react-bootstrap-icons';
 import './ActivityPostHome.css';
 
 function ActivityPostHome(props) {
     const { data } = props;
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    let postDate = new Date(data.postDate);
+    let expireDate = new Date(data.expires);
     return (
         <div className="activityContainer">
             <Alert variant="info" className='customAlert'>
@@ -11,10 +15,14 @@ function ActivityPostHome(props) {
                         <Image src={`http://localhost:3001/uploads/${data.user.img}`} alt="userImage" className="activityUserImage" roundedCircle />
                     </Col>
                     <Col md={10}>
-                        <Alert.Heading>{data.title}</Alert.Heading>
-                        <p>{data.description}</p>
+                        <h5 className='actUserName'>{data.user.name} {data.user.lastname} <span>posteó el {postDate.getDate()} de {meses[postDate.getMonth()]} de {postDate.getFullYear()}</span> </h5>
                         <hr />
-                        <p className="mb-0">{data.expires}</p>
+                        <Alert.Heading>{data.title}</Alert.Heading>
+                        <p>{data.description} {data.description} {data.description} {data.description} {data.description} {data.description} {data.description} {data.description} {data.description} </p>
+                        <div className="dateCheck">
+                            <h5 className='actExpireDate'>{expireDate.getDate()} de {meses[expireDate.getMonth()]} de {expireDate.getFullYear()}</h5>
+                            <CheckCircle color="grey" size={28} />
+                        </div>
                     </Col>
                 </Row>
 
