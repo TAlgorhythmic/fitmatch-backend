448b0fa66d3a0fe7a39451e71a52bccfdd4e6559
Adrián Vico <adrian.vico.95@gmail.com>
2024-08-07 17:28:30 +0200

    Añadida foto post

--- front/src/components/Home/ActivityPostHome.jsx
+++ front/src/components/Home/ActivityPostHome.jsx
@@ -1,4 +1,4 @@
-import { Alert, Row, Col } from 'react-bootstrap';
+import { Alert, Row, Col, Image } from 'react-bootstrap';
 import './ActivityPostHome.css';
 
 function ActivityPostHome(props) {
@@ -7,13 +7,17 @@ function ActivityPostHome(props) {
         <div className="activityContainer">
             <Alert variant="info" className='customAlert'>
                 <Row>
-                    <Col md={3}>
+                    <Col md={2}>
+                        <Image src={`http://localhost:3001/uploads/${data.user.img}`} alt="userImage" className="activityUserImage" roundedCircle />
+                    </Col>
+                    <Col md={10}>
+                        <Alert.Heading>{data.title}</Alert.Heading>
+                        <p>{data.description}</p>
+                        <hr />
+                        <p className="mb-0">{data.expires}</p>
                     </Col>
                 </Row>
-                <Alert.Heading>{data.title}</Alert.Heading>
-                <p>{data.description}</p>
-                <hr />
-                <p className="mb-0">{data.expires}</p>
+
             </Alert>
         </div>
     );
