a8fd3e77bd0e00479a83031445cae517f56901c2
Algorhythmics <intivioli@gmail.com>
2024-08-08 12:34:35 +0200

    Update.

--- front/src/components/Maps/LocationAutomplete.jsx
+++ front/src/components/Maps/LocationAutomplete.jsx
@@ -1,5 +1,12 @@
 import { useLoadScript, GoogleMap, Autocomplete } from "@react-google-maps/api"
 
+const libraries = ["places"];
+
 export default function InputLocationAutocomplete(props) {
-    
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.MAPS_API_KEY,
+        libraries: libraries
+    });
+
+    const [location, setLocation] = 
 }
