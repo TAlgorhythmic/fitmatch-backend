3e4985710df9c51bb106690be8178ae735205716
Adrián Vico <adrian.vico.95@gmail.com>
2024-08-08 16:49:24 +0200

    Iniciada agenda

---
+++ front/src/Agenda.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import BaseController from './controllers/BaseController';
+import { Row, Col, Container} from 'react-bootstrap';
+import './Agenda.css';
+
+function Agenda() {
+
+
+
+return (
+    <>
+    
+    </>
+)    
+}
+
+export default Agenda;
