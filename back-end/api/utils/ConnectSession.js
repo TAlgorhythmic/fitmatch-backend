dbc94ceda96b8350078eca0ca3d50183bab20a72
Algorhythmics <intivioli@gmail.com>
2024-08-08 16:32:24 +0200

    Reject endpoint.

--- back-end/api/utils/ConnectSession.js
+++ back-end/api/utils/ConnectSession.js
@@ -1,5 +1,6 @@
 import { sketchyOrder } from "../../routers/UsersRouter.js";
 import { buildInternalErrorPacket, buildInvalidPacket, buildSendDataPacket } from "../packets/PacketBuilder.js";
+import User from "../User.js";
 import fitmatch from "./../Fitmatch.js";
 import { areCompatible } from './Algorithms.js';
 
@@ -9,13 +10,27 @@ const TIMEOUT = 360000;
 export const sessions = new Map();
 
 class ConnectSession {
-    constructor(user) {
-        this.user = user;
+    constructor(id) {
+        this.id = id;
         this.position = 0;
         this.rejects = [];
         this.postRejects(20000);
         this.isCancelled = false;
         this.modified = new Date();
+        this.user = fitmatch.getUserManager().containsKey(this.id) ? fitmatch.userManager.get(this.id).user : undefined;
+        if (!this.user) {
+            fitmatch.sqlManager.getUserFromId(this.id)
+            .then(e => {
+                const data = e;
+                const user = new User(data.id, data.name, data.lastname, data.email, data.phone, data.description, data.proficiency, data.trainingPreferences, data.img, data.city, data.latitude, data.longitude, data.isSetup, data.monday, data.tuesday, data.wednesday, data.thursday, data.friday, data.saturday, data.sunday, data.timetable1, data.timetable2);
+                fitmatch.userManager.put(user.id, user);
+                this.user = user;
+            })
+            .catch(err => {
+                console.log(err);
+                console.warn("Fatal error warning! User did not exist and could not fetch it. Possible crash incoming.")
+            })
+        }
     }
 
     sendMore(response) {
@@ -56,7 +71,7 @@ class ConnectSession {
         setTimeout(async () => {
             try {
                 this.rejects.forEach(reject => {
-                    fitmatch.sqlManager.putRejection(this.user, reject)
+                    fitmatch.sqlManager.putRejection(this.user.id, reject)
                         .then(e => {
                             console.log("Rejects updated for " + this.user.email);
                             console.log("Output: " + e[0]);
