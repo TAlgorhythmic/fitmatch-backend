4e14e351bd72c18c759a5961d3aa3b0157bb306c
Algorhythmics <intivioli@gmail.com>
2024-08-11 00:23:03 +0200

    Add popup utils.

---
+++ front/src/Utils/PopupMessage.jsx
@@ -0,0 +1,19 @@
+import { Button } from "react-bootstrap";
+import "./style.css";
+
+export default function PopupMessage(props) {
+    const isVisible = props.isVisible;
+    const onClose = props.onClose;
+    const title = props.title;
+    const message = props.message;
+    const isError = props.isError;
+
+    return (
+        <div className={`popup${isVisible ? " visible" : ""}${isError ? " error" : ""}`}>
+            <div className="first-item flex"><h1 className="item">{title}</h1></div>
+            <div className="flex"><br className="separator item"></br></div>
+            <div className="flex">{message}</div>
+            <div className="last-item flex"><Button onClick={onClose}>Aceptar</Button></div>
+        </div>
+    )
+}
