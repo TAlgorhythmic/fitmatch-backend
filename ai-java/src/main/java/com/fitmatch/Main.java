145b618e103e6bbbaaebc67d50b484da66501473
Algorhythmics <intivioli@gmail.com>
2024-08-20 15:27:33 +0200

    Initial Commit.

---
+++ ai-java/src/main/java/com/fitmatch/Main.java
@@ -0,0 +1,4 @@
+package com.fitmatch;
+
+public class Main {
+}
---
+++ ai-java/src/main/java/com/fitmatch/utils/Urls.java
@@ -0,0 +1,27 @@
+package com.fitmatch.utils;
+
+public class Urls {
+
+    private static final String baseUrl = "http://localhost:3001";
+
+    private static final String authBaseUrl = baseUrl + "/api/auth";
+    private static final String activitiesBaseUrl = baseUrl + "/api/activities";
+    private static final String usersBaseUrl = baseUrl + "/api/users";
+    private static final String joinedActivitiesBaseUrl = baseUrl + "/api/joinedactivities";
+    private static final String requestsBaseUrl = baseUrl + "/api/requests";
+    private static final String credentialsBaseUrl = baseUrl + "/api/credentials";
+
+    public static final String ALL_ACTIVITIES;
+    public static final String FEED;
+    public static final String CREATE_ACTIVITY;
+    public static final String EDIT_ACTIVITY_BY_ID;
+    public static final String ACTIVITY_BY_ID;
+    public static final String GETOWN_ACTIVITIES;
+    public static final String DELETE_ACTIVITY_BY_ID;
+
+    public static final String LOGIN;
+    public static final String REGISTER;
+    public static final String VALIDATE_TOKEN;
+
+    
+}
