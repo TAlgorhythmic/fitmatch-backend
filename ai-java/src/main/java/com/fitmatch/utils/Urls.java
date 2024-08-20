84a592288d617c8089ea219b6966b6e7456a4681
Algorhythmics <intivioli@gmail.com>
2024-08-20 15:51:08 +0200

    Update.

--- ai-java/src/main/java/com/fitmatch/utils/Urls.java
+++ ai-java/src/main/java/com/fitmatch/utils/Urls.java
@@ -23,5 +23,16 @@ public class Urls {
     public static final String REGISTER;
     public static final String VALIDATE_TOKEN;
 
+    public static final String JOINED_ACTIVITIES_ALL;
+    public static final String LEAVE_ACTIVITY_BY_ID;
+    public static final String JOIN_ACTIVITY_BY_ID;
+    public static final String NOTJOINED_ACTIVITIES_ALL;
+
+    public static final String SEND_FRIEND_REQUEST_SWIPE;
+    public static final String ACCEPT_FRIEND_REQUEST_NOTIFICATION;
+    public static final String GET_PENDING_BY_ID;
+    public static final String GET_PENDINGS_OWN;
+    public static final String PENDING_CREATE;
+
     
 }
