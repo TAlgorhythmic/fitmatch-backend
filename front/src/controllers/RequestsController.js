d20739f92212a16c37521867d6c110da95c65f7d
Adrián Vico <adrian.vico.95@gmail.com>
2024-08-20 15:46:38 +0200

    cambios controllers

--- front/src/controllers/RequestsController.js
+++ front/src/controllers/RequestsController.js
@@ -4,28 +4,6 @@ class RequestsController extends BaseController {
     constructor(token) {
         super("requests", token);
     }
-
-    // Obtener lista de IDs de usuarios agregados
-    async getFriends() {
-        let data = [];
-        await fetch(`${this.apiUrl}/friends`, {
-            method: 'GET',
-            headers: {
-                "Authorization": "Bearer " + this.token,
-                'Content-Type': 'application/json'
-            }
-        }).then(res =>
-            res.json()
-                .then(responseData => {
-                    console.log('RequestControl: ' + responseData.data);
-                    data = responseData;
-                })
-                .catch(error => {
-                    console.error('Error getFeed:', error);
-                })
-        );
-        return data;
-    }
 }
 
 export default RequestsController;
--- front/src/controllers/UsersController.js
+++ front/src/controllers/UsersController.js
@@ -48,6 +48,28 @@ class UsersController extends BaseController {
         );
         return data;
     }
+
+    // Obtener lista de IDs de usuarios agregados
+    async getFriends() {
+        let data = [];
+        await fetch(`${this.apiUrl}/friends`, {
+            method: 'GET',
+            headers: {
+                "Authorization": "Bearer " + this.token,
+                'Content-Type': 'application/json'
+            }
+        }).then(res =>
+            res.json()
+                .then(responseData => {
+                    console.log('RequestControl: ' + responseData.data);
+                    data = responseData;
+                })
+                .catch(error => {
+                    console.error('Error getFeed:', error);
+                })
+        );
+        return data;
+    }
 }
 
 export default UsersController;
