5da89583b7fbbc9c7a06ef8be437883431dbdee4
Algorhythmics <intivioli@gmail.com>
2024-08-20 14:27:58 +0200

    Some fixes in home.

--- front/src/controllers/RequestsController.js
+++ front/src/controllers/RequestsController.js
@@ -5,26 +5,7 @@ class RequestsController extends BaseController {
         super("requests", token);
     }
 
-    // Obtener lista de IDs de usuarios agregados
-    async getFriends() {
-        let data = [];
-        await fetch(`${this.apiUrl}/friends`, {
-            method: 'GET',
-            headers: {
-                "Authorization": "Bearer " + this.token,
-                'Content-Type': 'application/json'
-            }
-        }).then(res =>
-            res.json()
-                .then(responseData => {
-                    data = responseData.data;
-                })
-                .catch(error => {
-                    console.error('Error getFeed:', error);
-                })
-        );
-        return data;
-    }
+    
 }
 
 export default RequestsController;
