45bec4686c0fee5e152565a8c792ef63ce932b09
Algorhythmics <intivioli@gmail.com>
2024-08-28 00:13:27 +0200

    Charset fixes

--- ai-java/src/main/java/com/fitmatch/core/fetch/controllers/AuthController.java
+++ ai-java/src/main/java/com/fitmatch/core/fetch/controllers/AuthController.java
@@ -4,6 +4,7 @@ import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 
@@ -24,7 +25,7 @@ public class AuthController {
 
             HttpPost post = new HttpPost(Urls.LOGIN);
             post.setHeader("Content-Type", "application/json");
-            post.setEntity(new StringEntity(packet.toJson()));
+            post.setEntity(new StringEntity(packet.toJson(), ContentType.APPLICATION_JSON.withCharset("UTF-8")));
 
             try (CloseableHttpResponse res = client.execute(post)) {
                 String json = EntityUtils.toString(res.getEntity(), "UTF-8");
@@ -50,7 +51,7 @@ public class AuthController {
 
             HttpPost post = new HttpPost(Urls.REGISTER);
             post.setHeader("Content-Type", "application/json");
-            post.setEntity(new StringEntity(packet.toJson()));
+            post.setEntity(new StringEntity(packet.toJson(), ContentType.APPLICATION_JSON.withCharset("UTF-8")));
 
             try (CloseableHttpResponse res = client.execute(post)) {
                 String json = EntityUtils.toString(res.getEntity(), "UTF-8");
--- ai-java/src/main/java/com/fitmatch/core/fetch/controllers/UsersController.java
+++ ai-java/src/main/java/com/fitmatch/core/fetch/controllers/UsersController.java
@@ -5,6 +5,7 @@ import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 
@@ -28,7 +29,7 @@ public class UsersController {
             HttpPost post = new HttpPost(Urls.SETUP);
             post.setHeader("Content-Type", "application/json");
             post.setHeader("Authorization", "Bearer " + token);
-            post.setEntity(new StringEntity(packet.toJson()));
+            post.setEntity(new StringEntity(packet.toJson(), ContentType.APPLICATION_JSON.withCharset("UTF-8")));
 
             try (CloseableHttpResponse res = client.execute(post)) {
                 String json = EntityUtils.toString(res.getEntity(), "UTF-8");
