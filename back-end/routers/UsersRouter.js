bafc624876e3baf82337ec3d5594cc8d4440949a
Algorhythmics <intivioli@gmail.com>
2024-09-09 23:36:46 +0200

    Fix

--- back-end/routers/UsersRouter.js
+++ back-end/routers/UsersRouter.js
@@ -114,7 +114,7 @@ export function sketchyOrder(array) {
     if (!array.length && !likelyMatch.length) return feed;
 
     if (likelyMatch.length) feed.push(likelyMatch.pop());
-    let i = 0;
+    let i = 1;
     while (array.length || likelyMatch.length) {
         if (i % 4 === 0 && likelyMatch.length) {
             const pop = likelyMatch.pop();
